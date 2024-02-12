4e465b4759532041c40903611b4e199c6e479fa0
chesswondo <chesswondo@gmail.com>
2024-02-12 20:13:39 +0200

    Add detection utils

--- assets/models/pieces_detection/rtmdet-tiny/rtmdet_tiny_8xb32-300e_coco_chess.py
+++ assets/models/pieces_detection/rtmdet-tiny/rtmdet_tiny_8xb32-300e_coco_chess.py
@@ -1,49 +1,5 @@
-_base_ = './rtmdet_s_8xb32-300e_coco.py'
-
-checkpoint = 'https://download.openmmlab.com/mmdetection/v3.0/rtmdet/cspnext_rsb_pretrain/cspnext-tiny_imagenet_600e.pth'  # noqa
-
-model = dict(
-    backbone=dict(
-        deepen_factor=0.167,
-        widen_factor=0.375,
-        init_cfg=dict(
-            type='Pretrained', prefix='backbone.', checkpoint=checkpoint)),
-    neck=dict(in_channels=[96, 192, 384], out_channels=96, num_csp_blocks=1),
-    bbox_head=dict(in_channels=96, feat_channels=96, exp_on_reg=False))
-
-train_pipeline = [
-    dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        type='CachedMosaic',
-        img_scale=(640, 640),
-        pad_val=114.0,
-        max_cached_images=20,
-        random_pop=False),
-    dict(
-        type='RandomResize',
-        scale=(1280, 1280),
-        ratio_range=(0.5, 2.0),
-        keep_ratio=True),
-    dict(type='RandomCrop', crop_size=(640, 640)),
-    dict(type='YOLOXHSVRandomAug'),
-    dict(type='RandomFlip', prob=0.5),
-    dict(type='Pad', size=(640, 640), pad_val=dict(img=(114, 114, 114))),
-    dict(
-        type='CachedMixUp',
-        img_scale=(640, 640),
-        ratio_range=(1.0, 1.0),
-        max_cached_images=10,
-        random_pop=False,
-        pad_val=(114, 114, 114),
-        prob=0.5),
-    dict(type='PackDetInputs')
-]
-
-train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
-
-
-'''_base_ = './rtmdet_tiny_8xb32-300e_coco.py'
+# Inherit and overwrite part of the config based on this config
+_base_ = 'mmdet::rtmdet/rtmdet_tiny_8xb32-300e_coco.py'
 
 data_root = 'dataset/chess_dataset/' # dataset root
 
@@ -53,7 +9,6 @@ train_num_workers = 1
 max_epochs = 20
 stage2_num_epochs = 1
 base_lr = 0.00008
-
 
 metainfo = {
     'classes': ('pieces', 'bishop', 'black-bishop', 'black-king', 'black-knight',
@@ -82,7 +37,6 @@ val_dataloader = dict(
 test_dataloader = val_dataloader
 
 val_evaluator = dict(ann_file=data_root + 'val.json')
-
 test_evaluator = val_evaluator
 
 model = dict(bbox_head=dict(num_classes=num_classes))
@@ -148,7 +102,4 @@ custom_hooks = [
 load_from = './assets/models/pieces_detection/rtmdet-tiny/rtmdet_tiny_8xb32-300e_coco_20220902_112414-78e30dcc.pth'
 
 train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
-val_cfg = dict(_delete_=True, type='mmrazor.SingleTeacherDistillValLoop')
-test_cfg = dict(_delete_=True, type='mmrazor.SingleTeacherDistillValLoop')
 visualizer = dict(vis_backends=[dict(type='LocalVisBackend'),dict(type='TensorboardVisBackend')])
-'''
---
+++ utils/pieces_detection/detection_utils.py
@@ -0,0 +1,81 @@
+import numpy as np
+from typing import List
+
+def intersection_over_union(bbox1: List[float], bbox2: List[float]) -> float:
+    """
+    Calculates the Intersection Over Union (IOU) metric for two bounding boxes.
+    Bounding boxes must be specified as [x_min, y_min, x_max, y_max].
+
+    : param bbox1: (List) - first bounding box coordinates.
+    : param bbox2: (List) - second bounding box coordinates.
+    : return: (float) - the IOU metric as a float.
+    """
+    assert (
+        len(bbox1) == 4 and len(bbox2) == 4
+    ), "Bounding boxes must be in the format: [x_min, y_min, x_max, y_max]"
+
+    # Determine the (x, y)-coordinates of the intersection rectangle
+    x_left = max(bbox1[0], bbox2[0])
+    y_top = max(bbox1[1], bbox2[1])
+    x_right = min(bbox1[2], bbox2[2])
+    y_bottom = min(bbox1[3], bbox2[3])
+
+    # Compute the area of intersection rectangle
+    intersection_area = max(x_right - x_left, 0) * max(y_bottom - y_top, 0)
+    if intersection_area == 0:
+        return 0.0
+
+    # Compute the area of both the prediction and ground-truth rectangles
+    area_bbox1 = (bbox1[2] - bbox1[0]) * (bbox1[3] - bbox1[1])
+    area_bbox2 = (bbox2[2] - bbox2[0]) * (bbox2[3] - bbox2[1])
+
+    # Compute the intersection over union by dividing the intersection area
+    # by the sum of both areas minus the intersection area
+    iou = intersection_area / float(area_bbox1 + area_bbox2 - intersection_area)
+
+    return iou
+
+
+def filter_detections(raw_result: dict, iou_threshold: float, score_threshold: float) -> dict:
+    '''
+    Receives detection result via DetInferencer and discards extra bboxes.
+    
+    : param raw_result: (dict) - raw predictions got from the model.
+    : param iou_threshold: (float) - max value of iou that is allowed in predictions.
+    : param score_threshold: (float) - min score value that is allowed in predictions.
+
+    : return: (dict) - selected predictions in the same format as input values.
+    '''
+
+    if not all([key in raw_result.keys() for key in ['predictions', 'visualization']]):
+        raise ValueError("Incorrect format. Must have keys 'predictions' and 'visualization'")
+
+    num_predictions = len(raw_result['predictions'][0]['scores'])
+    mask = np.full(num_predictions, True, dtype=np.bool_)
+    bboxes = np.array(raw_result['predictions'][0]['bboxes'])
+    scores = np.array(raw_result['predictions'][0]['scores'])
+    labels = np.array(raw_result['predictions'][0]['labels'])
+
+    # filter by iou
+    for i in range(num_predictions):
+        for j in range(i+1, num_predictions):
+            bbox1 = bboxes[i]
+            bbox2 = bboxes[j]
+            if intersection_over_union(bbox1, bbox2) > iou_threshold:
+                if scores[i] < scores[j]:
+                    scores[i] = 0.0
+                else:
+                    scores[j] = 0.0
+            
+    # filter by scores
+    for ind, score in enumerate(scores):
+        if score < score_threshold:
+            mask[ind] = False
+
+    # apply mask
+    result = raw_result.copy()
+    result['predictions'][0]['labels'] = list(labels[mask])
+    result['predictions'][0]['scores'] = list(scores[mask])
+    result['predictions'][0]['bboxes'] = list(bboxes[mask])
+
+    return result
