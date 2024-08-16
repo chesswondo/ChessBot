baab14755e6a1ba0f05167293a610da3d484a706
chesswondo <chesswondo@gmail.com>
2024-08-16 15:03:37 +0300

    Moved interface logic

--- src/interface/create_engine.py
+++ src/interface/create_engine.py
@@ -1,5 +1,5 @@
 from interface.interface_base import InterfaceBase
-from interface.tkinter.tkinter_parallel import TkinterParallel
+from interface.tkinter.interface_tkinter import InterfaceTkinter
 from utils.common_utils import load_config
 
 def create_interface_engine(config: dict) -> InterfaceBase:
@@ -13,4 +13,4 @@ def create_interface_engine(config: dict) -> InterfaceBase:
     interface_config = load_config(f'../assets/configs/interface/{config["interface"]["interface_type"]}/config.json')
 
     if config["interface"]["interface_type"] == "tkinter":
-        return TkinterParallel(interface_config)
+        return InterfaceTkinter(interface_config)
--- src/interface/tkinter/interface_tkinter.py
+++ src/interface/tkinter/interface_tkinter.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from typing import List
 from interface.interface_base import InterfaceBase
 from utils.interface_utils import ButtonValue, LabelValue
 
@@ -8,7 +7,7 @@ class InterfaceTkinter(InterfaceBase):
 
     def __init__(self, config: dict) -> None:
         '''
-        Initializes an instance of InterfaceTkinter.
+        Initializes an instance of TkinterParallel.
 
         : param config: (dict) - tkinter configuration object.
         
@@ -19,69 +18,111 @@ class InterfaceTkinter(InterfaceBase):
         self._font = config["font"]
         self._font_size = config["font_size"]
 
-    def _main_asking_menu(self, title: str, question: str, option_values: List[str]) -> str:
+    def run(self) -> None:
         '''
         Creates main asking menu for different questions.
 
-        : param title: (str) - title on the window.
-        : param question: (str) - question on the window.
-        : param option_values: (List[str]) - list of the values bounded with buttons and also their names.
-
-        : return: (str) - selected option.
+        : return: (None) - this function does not return any value.
         '''
         def on_close():
-            pass
-    
-        def submit_action(window):
+            self._is_running = False
             window.destroy()
 
+        def color_selection():
+            self._color = color_var.get()
+
+        def mode_selection():
+            self._mode = mode_var.get()
+
+        def toggle_pause_run():
+            self._pause = not self._pause
+            if switch_var.get() == "Pause":
+                switch_var.set("Run")
+            else:
+                switch_var.set("Pause")
+        
+        self._is_running = True
         window = tk.Tk()
-        window.title(title)
+        window.title(LabelValue.TITLE)
         window.geometry(self._window_size)
         window.resizable(False, False)
 
-        radio_var = tk.StringVar(value=option_values[0])
+        # Set up the color question
+        color_frame = tk.Frame(window)
+        color_frame.pack(side=tk.LEFT, padx=20, pady=20)
 
-        radio_buttons = []
-        num_options = len(option_values)
-        for i in range(num_options):
-            radio_button = tk.Radiobutton(window,
-                                          text=option_values[i],
-                                          variable=radio_var,
-                                          value=option_values[i],
-                                          font=(self._font, self._font_size))
-            radio_buttons.append(radio_button)
+        color_label = tk.Label(color_frame, text=LabelValue.COLOR_QUESTION)
+        color_label.pack(anchor=tk.W)
 
-        question_label = tk.Label(window, text=question, font=(self._font, self._font_size))
-        question_label.pack(pady=10)
+        color_values = [ButtonValue.WHITE, ButtonValue.BLACK]
+        color_var = tk.StringVar(value=color_values[0])  # default selection
+        self._color = color_var.get()
+        for i in range(2):
+            color_button = tk.Radiobutton(color_frame,
+                                          text=color_values[i],
+                                          variable=color_var,
+                                          value=color_values[i],
+                                          command=color_selection)
+            color_button.pack(anchor=tk.W)
 
-        for radio_button in radio_buttons:
-            radio_button.pack(pady=10)
+        # Set up the mode question
+        mode_frame = tk.Frame(window)
+        mode_frame.pack(side=tk.RIGHT, padx=20, pady=20)
 
-        submit_button = tk.Button(window, text="Submit", command=lambda: submit_action(window), font=(self._font, self._font_size))
-        submit_button.pack(pady=15)
+        mode_label = tk.Label(mode_frame, text=LabelValue.MODE_QUESTION)
+        mode_label.pack(anchor=tk.W)
+
+        mode_values = [ButtonValue.AUTO_MODE, ButtonValue.SPEECH_RECOGNITION, ButtonValue.DETECTION_MODE]
+        mode_var = tk.StringVar(value=mode_values[0])  # default selection
+        self._mode = mode_var.get()
+        for i in range(3):
+            mode_button = tk.Radiobutton(mode_frame,
+                                         text=mode_values[i],
+                                         variable=mode_var,
+                                         value=mode_values[i],
+                                         command=mode_selection)
+            mode_button.pack(anchor=tk.W)
+
+        # Add the Pause/Run switch
+        switch_frame = tk.Frame(window)
+        switch_frame.pack(side=tk.BOTTOM, pady=20)
+
+        self._pause = True
+        switch_var = tk.StringVar(value="Run")  # default state
+        switch_button = tk.Button(switch_frame, textvariable=switch_var, command=toggle_pause_run, width=10)
+        switch_button.pack()
 
         window.protocol("WM_DELETE_WINDOW", on_close)
         window.mainloop()
-
-        return radio_var.get()
-
+    
     def get_color(self) -> str:
         '''
-        Launch program interface and get color from user.
+        Gets color from user.
 
         : return: (str) - user choice of which color they play.
         '''
-        return self._main_asking_menu(LabelValue.COLOR_TITLE,
-                                     LabelValue.COLOR_QUESTION,
-                                     [ButtonValue.WHITE, ButtonValue.BLACK])
+        return self._color
     
     def get_program_mode(self) -> str:
         '''
-        Launch program interface and get program mode from user.
+        Gets program mode from user.
 
-        : return: (str) - user choice of program mode that will be run.
+        : return: (str) - user choice of which program mode to run.
         '''
-        return self._main_asking_menu(LabelValue.MODE_TITLE,
-                                     LabelValue.MODE_QUESTION,
-                                     [ButtonValue.AUTO_MODE, ButtonValue.SPEECH_RECOGNITION, ButtonValue.DETECTION_MODE])
+        return self._mode
+    
+    def is_on_pause(self) -> bool:
+        '''
+        Gets program state from user.
+
+        : return: (bool) - window state whether the program is paused or not.
+        '''
+        return self._pause
+    
+    def is_running(self) -> bool:
+        '''
+        Gets program state from user.
+
+        : return: (bool) - window state whether the program is running or not.
+        '''
+        return self._is_running
